ad3bc6731b74000c34a94aa38c1885b0747173a5
Dalton <lambertd757@gmail.com>
2023-01-03 22:36:10 -0500

    Player vs Player functionality is now working

--- tic-tac-toe/src/TicTacToe.js
+++ tic-tac-toe/src/TicTacToe.js
@@ -3,6 +3,7 @@ import Square from './Square';
 import './TicTacToeCSS.css';
 
 
+
 const TicTacToe = () => {
   const [player1, setPlayer1] = useState('player');
   const [player2, setPlayer2] = useState('computer');
@@ -10,7 +11,10 @@ const TicTacToe = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [currentPlayer, setCurrentPlayer] = useState('X');
   const [message, setMessage] = useState('');
-
+  const winningCombinations = [  [0, 1, 2], [3, 4, 5], [6, 7, 8],  // rows
+  [0, 3, 6], [1, 4, 7], [2, 5, 8],  // columns
+  [0, 4, 8], [2, 4, 6]  // diagonals
+];
 
   const handlePlayer1Click = () => {
     setPlayer1(player1 === 'player' ? 'computer' : 'player');
@@ -28,24 +32,24 @@ const TicTacToe = () => {
   };
 
   const handleSquareClick = (row, col) => {
-    if (!started) {
+    if (!started || player1 !== 'player' || player2 !== 'player') {
       return;
     }
     const index = row * 3 + col;
     if (board[index] === null) {
-      setBoard(prevBoard => {
-        const newBoard = [...prevBoard];
-        newBoard[index] = currentPlayer;
-        return newBoard;
-      });
+      // Create a new board with the new move
+      const newBoard = [...board];
+      newBoard[index] = currentPlayer;
+      setBoard(newBoard);
       // Check if the current player has won the game
-      if (checkWin(board)) {
+      if (checkWin(newBoard)) {
         setStarted(false);
         // You can display a message or do something else to indicate that the game has ended
         setMessage(`Player ${currentPlayer} has won the game!`);
       } else {
+        
         // check if the game has ended in a draw
-        if (!board.includes(null)) {
+        if (!newBoard.includes(null)) {
           setStarted(false);
           setMessage(`The game has ended in a draw!`);
         }
@@ -55,35 +59,15 @@ const TicTacToe = () => {
     setCurrentPlayer(prevPlayer => prevPlayer === 'X' ? 'O' : 'X');
   };
   
-
-
-
+  
+  
   const checkWin = (currentBoard) => {
-    console.log('checkWin called'); // add this line
-    // check rows
-    for (let i = 0; i < 8; i += 3) {
-      if (currentBoard[i] && currentBoard[i] === currentBoard[i + 1] && currentBoard[i] === currentBoard[i + 2]) {
-        console.log('returning true'); // add this line
+    for (let i = 0; i < winningCombinations.length; i++) {
+      const [a, b, c] = winningCombinations[i];
+      if (currentBoard[a] && currentBoard[a] === currentBoard[b] && currentBoard[a] === currentBoard[c]) {
         return true;
       }
     }
-    // check columns
-    for (let i = 0; i < 3; i++) {
-      if (currentBoard[i] && currentBoard[i] === currentBoard[i + 3] && currentBoard[i] === currentBoard[i + 6]) {
-        console.log('returning true'); // add this line
-        return true;
-      }
-    }
-    // check diagonals
-    if (currentBoard[0] && currentBoard[0] === currentBoard[4] && currentBoard[0] === currentBoard[8]) {
-      console.log('returning true'); // add this line
-      return true;
-    }
-    if (currentBoard[2] && currentBoard[2] === currentBoard[4] && currentBoard[2] === currentBoard[6]) {
-      console.log('returning true'); // add this line
-      return true;
-    }
-    console.log('returning false'); // add this line
     return false;
   }
   
