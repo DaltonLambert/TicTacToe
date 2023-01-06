c355cf38b867d14b6e298308c46d18ee825996ea
Dalton <lambertd757@gmail.com>
2023-01-05 23:43:30 -0500

    fixed error where computer was placing O's in squares that were not empty

--- tic-tac-toe/src/ComputerPlayer.js
+++ tic-tac-toe/src/ComputerPlayer.js
@@ -15,6 +15,10 @@ const getRandomMove = (board) => {
       // decide which function to use to get the next move (e.g. getRandomMove or getBestMove)
       // based on the current state of the board
       const index = getRandomMove(board);
+      if (board[index] !== null) {
+        // the selected square is not empty, return the original board
+        return board;
+      }
       // update the board with the computer's move
       const newBoard = [...board];
       newBoard[index] = 'O';
